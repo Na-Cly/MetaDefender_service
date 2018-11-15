e42e4ae923a94eeb9a2248883cfa26e7e7cd7003
Derek <dschmell91@gmail.com>
2018-11-15 14:57:51 -0500

    Create forms.py

---
+++ forms.py
@@ -0,0 +1,24 @@
+from django import forms
+
+class MetaDefenderConfigForm(forms.Form):
+    error_css_class = 'error'
+    required_css_class = 'required'
+    url = forms.CharField(required=True,
+                          label="Metadefender URL",
+                          widget=forms.TextInput(),
+                          initial='',
+                          help_text="Example: https://metadefender.localhost:8008")
+    use_proxy = forms.BooleanField(required=False,
+                                   label="Proxy",
+                                   initial=False,
+                                   help_text="Use proxy for connecting to MetaDefender service")
+    #added api key for metadefender
+    api_key = forms.CharField(required=True,
+                          label="Metadefender API Key",
+                          widget=forms.TextInput(),
+                          initial='',
+                          help_text="Example: "
+                                    "aaa1111bbb222ccc333f")
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault('label_suffix', ':')
+        super(MetaDefenderConfigForm, self).__init__(*args, **kwargs)
